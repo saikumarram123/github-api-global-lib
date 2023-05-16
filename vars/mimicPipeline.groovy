58f4d9c7a48bf4e959c6a6c3923bff46e831b839
Prashantped <108050027+Prashantped@users.noreply.github.com>
2023-05-16 10:46:22 +0530

    Update mimicPipeline.groovy

--- vars/mimicPipeline.groovy
+++ vars/mimicPipeline.groovy
@@ -9,13 +9,13 @@ def call(Map pipelineParams) {
                 sh './gradlew clean'
 
                 echo 'Compile pipelineParams.app'
-                sh './gradlew :pipelineParams.app:build'
+                sh './gradlew :${pipelineParams.app}:build'
                 
                 echo 'Compile pipelineParams.base'
-                sh './gradlew :pipelineParams.base:build'
+                sh './gradlew :${pipelineParams.base}:build'
                 
                 echo 'Compile pipelineParams.common'
-                sh './gradlew :pipelineParams.common:build'
+                sh './gradlew :${pipelineParams.common}:build'
                 
             }
         }
@@ -23,25 +23,25 @@ def call(Map pipelineParams) {
             stage ('test') {
                 steps {
                echo 'running tests on pipelineParams.app'
-               sh './gradlew cleanTest pipelineParams.app:test --stacktrace'
+               sh './gradlew cleanTest ${pipelineParams.app}:test --stacktrace'
                   
                echo 'running tests on pipelineParams.base'
-               sh './gradlew cleanTest pipelineParams.base:test --stacktrace'   
+               sh './gradlew cleanTest ${pipelineParams.base}:test --stacktrace'   
                   
                echo 'running tests on pipelineParams.common'
-               sh './gradlew cleanTest pipelineParams.common:test --stacktrace'    
+               sh './gradlew cleanTest ${pipelineParams.common}:test --stacktrace'    
                 }
             }
       stage('Package') {
         steps {
                 echo 'Building jars for app'
-                sh './gradlew :pipelineParams.app:bootJar --stacktrace'
+                sh './gradlew :${pipelineParams.app}:bootJar --stacktrace'
           
                 echo 'Building jars for pipelineParams.base'
-                sh './gradlew :pipelineParams.base:bootJar --stacktrace'
+                sh './gradlew :${pipelineParams.base}:bootJar --stacktrace'
           
                 echo 'Building jars for pipelineParams.common'
-                sh './gradlew :pipelineParams.common:bootJar --stacktrace'
+                sh './gradlew :${pipelineParams.common}:bootJar --stacktrace'
         post {
             failure {
                 mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
