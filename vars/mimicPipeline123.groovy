c3b7b027bc2b46754e0a6646f1dbcd9e36298960
saikumarram123 <61085482+saikumarram123@users.noreply.github.com>
2023-05-16 16:17:30 +0530

    Update mimicPipeline123.groovy

--- vars/mimicPipeline123.groovy
+++ vars/mimicPipeline123.groovy
@@ -1,110 +1,37 @@
 def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        stages {
+               stage('Compile') {
+            steps {
 
-    pipeline {
+                echo 'Clean previous build output'
+                sh './gradlew clean'
 
-        agent any
+                echo 'Compile pipelineParams.app'
+                sh "./gradlew :${pipelineParams.app}:build"
+                
+                echo 'Compile pipelineParams.base'
+                sh "./gradlew :${pipelineParams.base}:build"
+                
+                echo 'Compile pipelineParams.common'
+                sh "./gradlew :${pipelineParams.common}:build"
+                
+            }
+        }
 
-        stages {
-
-               stage('Compile') {
-
-            steps {
-
-
-
-
-                echo 'Clean previous build output'
-
-                sh './gradlew clean'
-
-
-
-
-                echo 'Compile pipelineParams.app'
-
-                sh './gradlew :app:compileJava :pipelineParams.app:compileTestJava --stacktrace'
-
-               
-
-                echo 'Compile pipelineParams.base'
-
-                sh './gradlew :base:compileJava :pipelineParams.base:compileTestJava --stacktrace'
-
-               
-
-                echo 'Compile pipelineParams.common'
-
-                sh './gradlew :common :pipelineParams.common:compileTestJava --stacktrace'
-
-               
-
-            }
-
-        }
-
-
-
-
-            stage ('test') {
-
-                steps {
-
-               echo 'running tests on pipelineParams.app'
-
-               sh './gradlew cleanTest pipelineParams.app:test --stacktrace'
-
-                 
-
-               echo 'running tests on pipelineParams.base'
-
-               sh './gradlew cleanTest pipelineParams.base:test --stacktrace'  
-
-                 
-
-               echo 'running tests on pipelineParams.common'
-
-               sh './gradlew cleanTest pipelineParams.common:test --stacktrace'    
-
-                }
-
-            }
-
-     /* stage('Package') {
-
-        steps {
-
-                echo 'Building jars for app'
-
-                sh './gradlew :pipelineParams.app:bootJar --stacktrace'
-
-         
-
-                echo 'Building jars for pipelineParams.base'
-
-                sh './gradlew :pipelineParams.base:bootJar --stacktrace'
-
-         
-
-                echo 'Building jars for pipelineParams.common'
-
-                sh './gradlew :pipelineParams.common:bootJar --stacktrace' 
-
-        post {
-
-            failure {
-
-                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
-
-            }
-
-        }
-
-        }
-
-    } 
-  */
-} 
-
+            stage ('test') {
+                steps {
+               echo 'running tests on pipelineParams.app'
+               sh "./gradlew cleanTest ${pipelineParams.app}:test --stacktrace"
+                  
+               echo 'running tests on pipelineParams.base'
+               sh "./gradlew cleanTest ${pipelineParams.base}:test --stacktrace"   
+                  
+               echo 'running tests on pipelineParams.common'
+               sh "./gradlew cleanTest ${pipelineParams.common}:test --stacktrace"    
+                }
+            }
 }
-
+}
 }
